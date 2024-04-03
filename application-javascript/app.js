c7f84e876640797c1959c17a976e986e1c22076d
Jayparkrighthere <zoeytwelve@gmail.com>
2024-04-03 10:55:37 -0600

    update gateway service

--- application-javascript/app.js
+++ application-javascript/app.js
@@ -35,7 +35,7 @@ const port = 3001; // Port number to listen on
 app.use(express.json()); // Middleware to parse JSON bodies
 
 
-async function QueryTransactionByPoll(pollTableID) {
+async function GetPollTableHistory(pollTableID) {
 	const ccp = buildCCPOrg1();
 	const caClient = buildCAClient(FabricCAServices, ccp, 'ca.org1.example.com');
 	const wallet = await buildWallet(Wallets, walletPath);
@@ -52,8 +52,8 @@ async function QueryTransactionByPoll(pollTableID) {
 		const network = await gateway.getNetwork(channelName);
 		const contract = network.getContract(chaincodeName);
 
-		console.log('\n--> Evaluate Transaction: QueryTransactionByPoll, function returns an asset with a given assetID');
-		let result = await contract.evaluateTransaction('QueryTransactionByPoll', pollTableID);
+		console.log('\n--> Evaluate Transaction: GetPollTableHistory, function returns an asset with a given assetID');
+		let result = await contract.evaluateTransaction('GetPollTableHistory', pollTableID);
 		return JSON.parse(result.toString());
 
 	} finally {
@@ -63,7 +63,7 @@ async function QueryTransactionByPoll(pollTableID) {
 	}
 }
 
-async function CreateTransaction(assetID, transaction_type, poll_table_id, user_id, time_stamp) {
+async function CreatePoll(poll_table_id, user_id, time_stamp) {
 	const ccp = buildCCPOrg1();
 	const caClient = buildCAClient(FabricCAServices, ccp, 'ca.org1.example.com');
 	const wallet = await buildWallet(Wallets, walletPath);
@@ -80,8 +80,66 @@ async function CreateTransaction(assetID, transaction_type, poll_table_id, user_
 		const network = await gateway.getNetwork(channelName);
 		const contract = network.getContract(chaincodeName);
 
-		console.log('\n--> Submit Transaction: CreateTransaction, function returns an asset with a given assetID');
-		await contract.submitTransaction('CreateTransaction', assetID, transaction_type, poll_table_id, user_id, time_stamp);
+		console.log('\n--> Submit Transaction: CreatePoll, function returns an asset with a given assetID');
+		await contract.submitTransaction('CreatePoll', poll_table_id, poll_table_id, user_id, time_stamp);
+		console.log("result submitted ****");
+		return "Transaction Created Successfully";
+
+	} finally {
+		// Disconnect from the gateway when the application is closing
+		// This will close all connections to the network
+		gateway.disconnect();
+	}
+}
+
+async function AddVoteTransaction(pid, user_id) {
+	const ccp = buildCCPOrg1();
+	const caClient = buildCAClient(FabricCAServices, ccp, 'ca.org1.example.com');
+	const wallet = await buildWallet(Wallets, walletPath);
+	await enrollAdmin(caClient, wallet, mspOrg1);
+	await registerAndEnrollUser(caClient, wallet, mspOrg1, org1UserId, 'org1.department1');
+	const gateway = new Gateway();
+	try {
+		await gateway.connect(ccp, {
+			wallet,
+			identity: org1UserId,
+			discovery: { enabled: true, asLocalhost: true } // using asLocalhost as this gateway is using a fabric network deployed locally
+		});
+
+		const network = await gateway.getNetwork(channelName);
+		const contract = network.getContract(chaincodeName);
+
+		console.log('\n--> Submit Transaction: AddVoteTransaction');
+		await contract.submitTransaction('AddVoteTransaction', pid, user_id);
+		console.log("result submitted ****");
+		return "Transaction Created Successfully";
+
+	} finally {
+		// Disconnect from the gateway when the application is closing
+		// This will close all connections to the network
+		gateway.disconnect();
+	}
+}
+
+async function UpdatePoll(pid, newState) {
+	const ccp = buildCCPOrg1();
+	const caClient = buildCAClient(FabricCAServices, ccp, 'ca.org1.example.com');
+	const wallet = await buildWallet(Wallets, walletPath);
+	await enrollAdmin(caClient, wallet, mspOrg1);
+	await registerAndEnrollUser(caClient, wallet, mspOrg1, org1UserId, 'org1.department1');
+	const gateway = new Gateway();
+	try {
+		await gateway.connect(ccp, {
+			wallet,
+			identity: org1UserId,
+			discovery: { enabled: true, asLocalhost: true } // using asLocalhost as this gateway is using a fabric network deployed locally
+		});
+
+		const network = await gateway.getNetwork(channelName);
+		const contract = network.getContract(chaincodeName);
+
+		console.log('\n--> Submit Transaction: UpdatePoll, function returns an asset with a given assetID');
+		await contract.submitTransaction('UpdatePoll', pid, newState);
 		console.log("result submitted ****");
 		return "Transaction Created Successfully";
 
@@ -109,8 +167,37 @@ async function CreateVote(id, poll_table_id, candidate_id, time_stamp) {
 		const network = await gateway.getNetwork(voteChannelName);
 		const contract = network.getContract(voteChaincodeName);
 
-		console.log('\n--> Submit Vote: CreateVote, function returns an asset with a given assetID');
+		console.log('\n--> Submit Vote: CreateVote');
 		await contract.submitTransaction('CreateVote', id, poll_table_id, candidate_id, time_stamp);
+		console.log("result submitted ****");
+		return "Vote Created Successfully";
+
+	} finally {
+		// Disconnect from the gateway when the application is closing
+		// This will close all connections to the network
+		gateway.disconnect();
+	}
+}
+
+async function AddVote(pid) {
+	const ccp = buildCCPOrg1();
+	const caClient = buildCAClient(FabricCAServices, ccp, 'ca.org1.example.com');
+	const wallet = await buildWallet(Wallets, walletPath);
+	await enrollAdmin(caClient, wallet, mspOrg1);
+	await registerAndEnrollUser(caClient, wallet, mspOrg1, org1UserId, 'org1.department1');
+	const gateway = new Gateway();
+	try {
+		await gateway.connect(ccp, {
+			wallet,
+			identity: org1UserId,
+			discovery: { enabled: true, asLocalhost: true } // using asLocalhost as this gateway is using a fabric network deployed locally
+		});
+
+		const network = await gateway.getNetwork(voteChannelName);
+		const contract = network.getContract(voteChaincodeName);
+
+		console.log('\n--> Submit Vote: AddVote');
+		await contract.submitTransaction('AddVote', pid);
 		console.log("result submitted ****");
 		return "Vote Created Successfully";
 
@@ -153,7 +240,30 @@ async function QueryVoteByPoll(pollTableID) {
 app.get('/transactions', async (req, res) => {
 	try {
 		const pollID = req.query.pollID;
-		let a = await QueryTransactionByPoll(pollID);
+		let r = await GetPollTableHistory(pollID);
+
+		// Extract data from the request and prepare the response
+		// const responseData = {
+		// 	message: `*** Result:${r}`,
+		// 	timestamp: new Date()
+		// };
+
+		// Send the response
+		res.json(r);
+	} catch (error) {
+		// Handle any errors that occur during the asynchronous operation
+		console.error('An error occurred:', error);
+		res.status(500).json({ error: 'Internal Server Error' });
+	}
+});
+
+
+app.post('/create-poll', async (req, res) => {
+	try {
+		const requestData = req.body;
+
+		console.log("Request Data", requestData);
+		let a = await CreatePoll(requestData.poll_table_id, requestData.user_id, requestData.time_stamp);
 
 		// Extract data from the request and prepare the response
 		const responseData = {
@@ -170,17 +280,38 @@ app.get('/transactions', async (req, res) => {
 	}
 });
 
-
-app.post('/create-transaction', async (req, res) => {
+app.post('/add-transaction-vote', async (req, res) => {
 	try {
 		const requestData = req.body;
 
 		console.log("Request Data", requestData);
-		let a = await CreateTransaction(requestData.assetID, requestData.transaction_type, requestData.poll_table_id, requestData.user_id, requestData.time_stamp);
+		let result = await AddVoteTransaction(requestData.poll_table_id, requestData.user_id);
 
 		// Extract data from the request and prepare the response
 		const responseData = {
-			message: `*** Result:${a}`,
+			message: `*** Result:${result}`,
+			timestamp: new Date()
+		};
+
+		// Send the response
+		res.json(responseData);
+	} catch (error) {
+		// Handle any errors that occur during the asynchronous operation
+		console.error('An error occurred:', error);
+		res.status(500).json({ error: 'Internal Server Error' });
+	}
+});
+
+app.post('/update-poll', async (req, res) => {
+	try {
+		const requestData = req.body;
+
+		console.log("Request Data", requestData);
+		let result = await UpdatePoll(requestData.poll_table_id, requestData.newState);
+
+		// Extract data from the request and prepare the response
+		const responseData = {
+			message: `*** Result:${result}`,
 			timestamp: new Date()
 		};
 
@@ -220,7 +351,29 @@ app.post('/create-vote', async (req, res) => {
 		const requestData = req.body;
 
 		console.log("Request Data", requestData);
-		let a = await CreateVote(requestData.id, requestData.poll_table_id, requestData.candidate_id, requestData.time_stamp);
+		let r = await CreateVote(requestData.id, requestData.poll_table_id, requestData.candidate_id, requestData.time_stamp);
+
+		// Extract data from the request and prepare the response
+		const responseData = {
+			message: `*** Result:${r}`,
+			timestamp: new Date()
+		};
+
+		// Send the response
+		res.json(responseData);
+	} catch (error) {
+		// Handle any errors that occur during the asynchronous operation
+		console.error('An error occurred:', error);
+		res.status(500).json({ error: 'Internal Server Error' });
+	}
+});
+
+app.post('/vote', async (req, res) => {
+	try {
+		const requestData = req.body;
+
+		console.log("Request Data", requestData);
+		let a = await AddVote(requestData.id);
 
 		// Extract data from the request and prepare the response
 		const responseData = {
